195b42e66fde223a8831ceee9d857d68a1d82b99
Santiago Largo Botero <salargo-@c2r5s6.42madrid.com>
2025-10-06 20:48:07 +0200

    lsnew ok

--- ft_lstnew_bonus.c
+++ ft_lstnew_bonus.c
@@ -6,7 +6,7 @@
 /*   By: salargo- <salargo-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 16:00:46 by salargo-          #+#    #+#             */
-/*   Updated: 2025/10/06 20:39:19 by salargo-         ###   ########.fr       */
+/*   Updated: 2025/10/06 20:47:53 by salargo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,15 @@
 
 t_list	*ft_lstnew(void *content)
 {
+	t_list	*new_n;
+
+	new_n = (t_list *)malloc(sizeof(t_list));
+	if (!new_n)
+	{
+		free(new_n);
+		return (free(new_n), (NULL)); //try
+	}
+	new_n->content = (void *)content;
+	new_n->next = NULL;
+	return (new_n);
 }
