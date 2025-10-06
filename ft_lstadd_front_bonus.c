63b6c6203d62c7bedcc2c6c423b4ff45f4ccb912
Santiago Largo Botero <salargo-@c2r5s6.42madrid.com>
2025-10-06 20:54:27 +0200

    lstadd_front

--- ft_lstadd_front_bonus.c
+++ ft_lstadd_front_bonus.c
@@ -1,12 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstadd_front.c                                  :+:      :+:    :+:   */
+/*   ft_lstadd_front_bonus.c                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: salargo- <salargo-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 16:00:31 by salargo-          #+#    #+#             */
-/*   Updated: 2025/09/29 16:00:32 by salargo-         ###   ########.fr       */
+/*   Updated: 2025/10/06 20:53:00 by salargo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void	ft_lstadd_front(t_list **lst, t_list *new)
+{
+	if (lst && new)
+	{
+		new->next = *lst;
+		*lst = new;
+	}
+}
